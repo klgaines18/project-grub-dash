c59292ef3542af5ee17995184a686043b4681da2
Katie Gaines <klgaines18@gmail.com>
2023-02-16 11:37:25 -0500

    added functions to order controller

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,169 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+// MAIN FUNCTIONS //
+
+function list(req, res) {
+  res.json({ data: orders });
+}
+
+function read(req, res, next) {
+  res.json({ data: res.locals.order });
+};
+
+function create(req, res) {
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+  const newOrder = {
+    id: nextId(), 
+    deliverTo,
+    mobileNumber,
+    status,
+    dishes
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
+
+function update(req, res) {
+  const order = res.locals.order;
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+
+  // Update the order
+  order.deliverTo = deliverTo
+  order.mobileNumber = mobileNumber
+  order.status = status
+  order.dishes = dishes
+
+  res.json({ data: order });
+}
+
+function destroy(req, res) {
+  const { orderId } = req.params;
+  const index = orders.findIndex((order) => order.id === orderId);
+  // `splice()` returns an array of the deleted elements, even if it is one element
+  const deletedOrder = orders.splice(index, 1);
+  res.sendStatus(204);
+}
+
+
+// VALIDATION //
+
+function orderExists(req, res, next) {
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => order.id === orderId);
+  if (foundOrder) {
+    res.locals.order = foundOrder;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Order id not found: ${orderId}`,
+  });
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Order must include a ${propertyName}` });
+  };
+}
+
+function dishesPropertyIsValid(req, res, next) {
+  const { data: { dishes } = {} } = req.body;
+  if (!Array.isArray(dishes) || dishes.length <= 0) {
+    return next({
+      status: 400,
+      message: `Order must include at least one dish`
+    });
+  }
+  next()
+}
+
+function dishesQuantityIsValid(req, res, next) {
+  const { data: { dishes } = {} } = req.body;
+  dishes.forEach((dish, index) => {
+    const quantity = dish.quantity
+    if (!quantity || quantity <= 0 || !Number.isInteger(quantity)) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`
+      });
+    }
+  })
+  next()
+}
+
+function orderIdMatches(req, res, next) {
+  const { orderId } = req.params;
+  const { data: { id } = {} } = req.body;
+  if (id) {
+    if (id !== orderId) {
+      return next({
+          status: 400,
+          message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`
+      });
+    }
+  }
+  next()
+}
+
+function statusPropertyIsValid(req, res, next) {
+  const { data: { status } = {} } = req.body;
+  const validStatus = ["pending", "preparing", "out-for-delivery" ];
+  if (validStatus.includes(status)) {
+    return next();
+  }
+  if (status === "delivered") {
+    return next({
+      status: 400,
+      message: `A delivered order cannot be changed`,
+    });
+  }
+  next({
+    status: 400,
+    message: `Order must have a status of pending, preparing, out-for-delivery, delivered`,
+  });
+}
+
+function statusPropertyIsPending(req, res, next) {
+  const order = res.locals.order;
+  if (order.status !== "pending") {
+    return next({
+      status: 400,
+      message: `An order cannot be deleted unless it is pending`,
+    });
+  }
+  next()
+}
+
+
+// EXPORTS //
+
+module.exports = {
+  list,
+  read: [orderExists, read],
+  create: [
+    bodyDataHas("deliverTo"),
+    bodyDataHas("mobileNumber"),
+    bodyDataHas("dishes"),
+    dishesPropertyIsValid,
+    dishesQuantityIsValid,
+    create
+  ],
+  update: [
+    orderExists,
+    orderIdMatches,
+    statusPropertyIsValid,
+    bodyDataHas("deliverTo"),
+    bodyDataHas("mobileNumber"),
+    bodyDataHas("dishes"),
+    dishesPropertyIsValid,
+    dishesQuantityIsValid,
+    update
+  ],
+  delete: [orderExists, statusPropertyIsPending, destroy],
+}
