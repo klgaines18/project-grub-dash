aa58beda65973434cf3a121637557f73e4e53b68
Katie Gaines <klgaines18@gmail.com>
2023-02-16 09:51:15 -0500

    added functions to dishes controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,113 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+
+// MAIN FUNCTIONS //
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function read(req, res, next) {
+  res.json({ data: res.locals.dish });
+};
+
+function create(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId(), // Increment last id then assign as the current ID
+    name,
+    description,
+    price,
+    image_url
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function update(req, res) {
+  const dish = res.locals.dish;
+  const { data: { name, description, price, image_url } = {} } = req.body;
+
+  // Update the paste
+  dish.name = name;
+  dish.description = description;
+  dish.price = price;
+  dish.image_url = image_url;
+
+  res.json({ data: dish });
+}
+
+// VALIDATION //
+
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find(dish => dish.id === Number(dishId));
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Dish does not exist: ${dishId}`,
+  });
+};
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Dish must include a ${propertyName}` });
+  };
+}
+
+function priceIsValidNumber(req, res, next){
+  const { data: { price }  = {} } = req.body;
+  if (price <= 0 || !Number.isInteger(price)){
+      return next({
+          status: 400,
+          message: `Dish must have a price that is an integer greater than 0`
+      });
+  }
+  next();
+}
+
+function dishIdMatches(req, res, next) {
+  const { dishId } = req.params;
+  const { data: { id } = {} } = req.body;
+  if (id !== Number(dishId)) {
+    return next({
+        status: 400,
+        message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`
+    });
+  }
+  next()
+}
+
+// EXPORTS //
+
+module.exports = {
+  list,
+  read: [dishExists, read],
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    priceIsValidNumber,
+    create
+  ],
+  update: [
+    dishExists,
+    dishIdMatches,
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    priceIsValidNumber,
+    update
+  ],
+};
